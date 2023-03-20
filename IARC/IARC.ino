1d4538ba94feac5074ffddeb3fd9c5b33b292699
gaurav136 <gauravtale136@gmail.com>
2023-03-20 22:42:25 +0530

    I added PID line foller
    It is a pid line follower code for sharp turn

--- IARC/IARC.ino
+++ IARC/IARC.ino
@@ -1,12 +1,10 @@
-
-
 #define Kp 1 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
 #define Kd 100// experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
 #define MaxSpeed 255// max speed of the robot
 #define BaseSpeed 150 // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define SensorCount  8     // number of sensors used
 
-#define speedturn 180
+//#define speedturn 180
 
 //#define rightMotor1 5
 //#define rightMotor2 6
