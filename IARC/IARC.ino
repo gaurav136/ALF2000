a8ba84e84f71d372fbc38b67efd6116fc970b21c
gaurav136 <gauravtale136@gmail.com>
2023-03-25 09:52:09 +0530

    PID line following , sharp turn, Obstracal detection & avoidance

--- IARC/IARC.ino
+++ IARC/IARC.ino
@@ -1,37 +1,50 @@
 #include <SharpIR.h>
-#define Kp 1 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 100// experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define Kp 0.20  // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 0 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define Ki 0
+
+
 #define MaxSpeed 255// max speed of the robot
-#define BaseSpeed 150 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define BaseSpeed 255 // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define SensorCount  8     // number of sensors used
 
+#define speedturn 50
 
-#define ir A8
+#define ena  5
+#define in1  6
+#define in2  7
+#define in3  8
+#define in4  9
+#define enb  10
+
+
+#define ir A6
 #define model 1080
-// ir: the pin where your sensor is attached
-// model: an int that determines your sensor:  1080 for GP2Y0A21Y
-//                                            20150 for GP2Y0A02Y
-//                                            (working distance range according to the datasheets)
 SharpIR SharpIR(ir, model);
 
-
-#define speedturn 180
-
+int P;
+int I;
+int D;
 int rightMotorSpeed;
 int leftMotorSpeed;
-int ena = 7;
-int in1 = 6;
-int in2 = 5;
-int in3 = 4;
-int in4 = 3;
-int enb = 2;
 
-const uint8_t SensorPins[SensorCount] = {A7,A6,A5,A4,A3,A2,A1,A0};
+const uint8_t SensorPins[SensorCount] = {A0, A1, A2, A3, A4, A5, 3, 2};
+
 //unsigned int sensorValues[SensorCount];
 
 int lastError = 0;
 unsigned int Sensor[8];
 
+int botPosition() {
+  int Position = 0;
+  int sensor = 0;
+  for (int i = 0 ; i < SensorCount; i++) {
+    Position += i * 1000 * digitalRead(SensorPins[i]);
+    sensor += digitalRead(SensorPins[i]);
+  }
+  Position /= sensor;
+  return Position ;
+}
 
 void move(int motor, int Speed, int Direction) {
   //digitalWrite(motorPower, HIGH); //disable standby
@@ -52,21 +65,26 @@ void move(int motor, int Speed, int Direction) {
     digitalWrite(in1, inPin1);
     digitalWrite(in2, inPin2);
     analogWrite(ena, Speed);
+//        Serial.print("Speed0 :");
+//        Serial.println(Speed);
   }
   if (motor == 1) {
     digitalWrite(in3, inPin1);
     digitalWrite(in4, inPin2);
     analogWrite(enb, Speed);
+//        Serial.print("Speed1 :");
+//        Serial.println(Speed);
   }
+
+  //  delay(2000);
+
 }
-
-
 
 
 void setup()
 {
-  Serial.begin(115200);
-  
+ // Serial.begin(9600);
+
   for (int i = 0; i < SensorCount ; i++) {
     pinMode(SensorPins[i], INPUT);
   }
@@ -77,76 +95,106 @@ void setup()
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(enb, OUTPUT);
-  //pinMode(motorPower, OUTPUT);
 
-  //wait();
-  // wait for 2s to position the bot before entering the main loop
+
+  delay(2000); // wait for 2s to position the bot before entering the main loop
 }
 
 
 
 void loop()
 {
-//    for(int i =0; i<8; i++){
-//      Serial.print(i);
-//      Serial.print(" :");
-//      Serial.println(digitalRead(SensorPins[i]));
-//    }
-//
-//    Serial.println("");
-//    delay(1000);
-//
-//    return;
+  //   for(int i =0; i<8; i++){
+  //      Serial.print(i);
+  //      Serial.print(" :");
+  //      Serial.println(digitalRead(SensorPins[i]));
+  //    }
+  //
+  //    Serial.println("");
+  //    delay(1000);
+  //
+  //    return;
 
+  int Position = botPosition();
+  //  Serial.println(Position);
+  //  delay(500);
+  //  return;
 
-  int Position = 0;
-  int sensor = 0;
-  for (int i = 0 ; i < SensorCount; i++) {
-    Position += i * 1000 * digitalRead(SensorPins[i]);
-    sensor += digitalRead(SensorPins[i]);
-  }
-  Position /= sensor;
-  // Serial.println(Position);
-  // delay(500);
-  // return;
+  int dis = SharpIR.distance();
+  //Serial.println(dis);
 
-  
-  int dis=SharpIR.distance();
-  Serial.println(dis);
-  
-  if ( dis <= 13) {
-    Serial.println(dis);
-    while (Position > 6500) {
-      move(0, speedturn, 1);
-      move(1, speedturn, 0);
+  if ( dis <= 8) {
+    //Serial.println(dis);
+    while (Position < 6500) {
+      Position = botPosition();
+      //Serial.println(Position);
+      move(1, speedturn, 1);
+      move(0, speedturn, 0);
+      // delay(2000);
     }
     return;
   }
 
-  if (Position < 400) {
+
+  if (Position == -1) {
     //    move(0, speedturn, 1);
     //    move(1, speedturn, 1);
-    move(0, rightMotorSpeed, 1);
-    move(1, leftMotorSpeed, 1);
-    return;
+    if (rightMotorSpeed > leftMotorSpeed) {
+      leftMotorSpeed = 0;
+      move(1, rightMotorSpeed, 1);
+      move(0, leftMotorSpeed, 1);
+//            Serial.println("rightMotorSpeed : ");
+//            Serial.println(rightMotorSpeed);
+//            Serial.println("leftMotorSpeed : ");
+//            Serial.println(leftMotorSpeed);
+//            delay(2000);
+      return;
+    }
+    if (rightMotorSpeed < leftMotorSpeed) {
+      rightMotorSpeed = 0;
+      move(1, rightMotorSpeed, 1);
+      move(0, leftMotorSpeed, 1);
+//            Serial.println("rightMotorSpeed : ");
+//            Serial.println(rightMotorSpeed);
+//            Serial.println("leftMotorSpeed : ");
+//            Serial.println(leftMotorSpeed);
+//            delay(2000);
+      return;
+    }
+
   }
 
-  //int error = Position ;
   //int  error = Position - (SensorCount-1)*1000/2;
   int  error = Position - 3500;
-  int motorSpeed = Kp * error + Kd * (error - lastError);
+  P = error;
+  I = I + error;
+  D = error - lastError;
+  int motorSpeedDiff = Kp * P + Kd * D + Ki * I;
   lastError = error;
-  //  Serial.println(error);
+  //  Serial.println(motorSpeedDiff);
   //  return;
 
-  rightMotorSpeed = BaseSpeed + motorSpeed;
-  leftMotorSpeed = BaseSpeed - motorSpeed;
+  rightMotorSpeed = BaseSpeed + motorSpeedDiff;
+  leftMotorSpeed = BaseSpeed - motorSpeedDiff;
+  //  Serial.println("rightMotorSpeed :");
+  //  Serial.println(rightMotorSpeed);
+  //  Serial.println("leftMotorSpeed : ");
+  //  Serial.println(leftMotorSpeed);
+  //  delay(2000);
+  //  return;
 
   if (rightMotorSpeed > MaxSpeed ) rightMotorSpeed = MaxSpeed; // prevent the motor from going beyond max speed
   if (leftMotorSpeed > MaxSpeed ) leftMotorSpeed = MaxSpeed; // prevent the motor from going beyond max speed
   if (rightMotorSpeed < 0)rightMotorSpeed = 0;
   if (leftMotorSpeed < 0)leftMotorSpeed = 0;
 
-  move(0, rightMotorSpeed, 1);
-  move(1, leftMotorSpeed, 1);
+  //  Serial.println("rightMotorSpeed : ");
+  //  Serial.println(rightMotorSpeed);
+  //  Serial.println("leftMotorSpeed : ");
+  //  Serial.println(leftMotorSpeed);
+  //  delay(2000);
+  //  return;
+
+  move(1, rightMotorSpeed, 1);
+  move(0, leftMotorSpeed, 1);
 }
